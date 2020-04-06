8b949ee8474cad5d33499c0e68d91b39c8942b4b
David Arvelo <david@davidarvelo.com>
2020-04-06 05:48:45 -0400

    Fix meta tag comma

--- server/render-html.js
+++ server/render-html.js
@@ -12,7 +12,7 @@ export default (
     <html ${head.htmlAttributes.toString()}>
       <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
         <!--[if IE]>
           <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
         <![endif]-->
