2081e4361731a9ce44bf7de84d4d38eaef33c01e
David Arvelo <david@davidarvelo.com>
2020-04-06 05:05:54 -0400

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "ignorePatterns": [
+        "node_modules/",
+        "dist/",
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
