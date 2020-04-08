856ae5f852a594986bb451cc052c46b0e564ca08
David Arvelo <david@davidarvelo.com>
2020-04-08 04:24:02 -0400

    Fix indentation

--- server/render-html.js
+++ server/render-html.js
@@ -4,46 +4,46 @@ const isDev = process.env.NODE_ENV === 'development';
 
 
 export default (
-  head,
-  htmlContent
+    head,
+    htmlContent
 ) => {
-  const html = `
-    <!doctype html>
-    <html ${head.htmlAttributes.toString()}>
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-        <!--[if IE]>
-          <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-        <![endif]-->
+    const html = `
+        <!doctype html>
+        <html ${head.htmlAttributes.toString()}>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+                <!--[if IE]>
+                    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+                <![endif]-->
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="shortcut icon" href="/favicon.ico">
+                <link rel="apple-touch-icon" href="apple-touch-icon.png">
+                <link rel="shortcut icon" href="/favicon.ico">
 
-        ${head.title.toString()}
-        <meta charset="utf-8"/>
-        ${head.meta.toString()}
-        ${head.link.toString()}
-      </head>
-      <body>
-        ${htmlContent}
+                ${head.title.toString()}
+                <meta charset="utf-8"/>
+                ${head.meta.toString()}
+                ${head.link.toString()}
+            </head>
+            <body>
+                ${htmlContent}
 
-        <!-- Insert bundled scripts into <script> tag -->
-        ${head.script.toString()}
-      </body>
-    </html>
-  `;
+                <!-- Insert bundled scripts into <script> tag -->
+                ${head.script.toString()}
+            </body>
+        </html>
+    `;
 
-  // html-minifier configuration, refer to "https://github.com/kangax/html-minifier" for more configuration
-  const minifyConfig = {
-    collapseWhitespace: true,
-    removeComments: true,
-    trimCustomFragments: true,
-    minifyCSS: true,
-    minifyJS: true,
-    minifyURLs: true,
-  };
+    // html-minifier configuration, refer to "https://github.com/kangax/html-minifier" for more configuration
+    const minifyConfig = {
+        collapseWhitespace: true,
+        removeComments: true,
+        trimCustomFragments: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+    };
 
-  // Minify html in production
-  return isDev ? html : minify(html, minifyConfig);
+    // Minify html in production
+    return isDev ? html : minify(html, minifyConfig);
 };
